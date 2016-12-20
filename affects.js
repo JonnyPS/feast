84b75db77af23b53c7e0601850fead7338c8a124
jonnyps <jonny_spry@msn.com>
2016-12-20 19:08:00 +0000

    commit files before tifying code

--- affects.js
+++ affects.js
@@ -1,7 +1,22 @@
 console.log('affects.js');
-$( ".cornerImg" ).hover(
+$( ".cornerImg" ).mouseover(
   // console.log('hello');
     function() {
-    TweenMax.to($( this ), 0.5, {x:200});
+    TweenMax.to($( this ), 0.1, {alpha:1, delay:0});
+    TweenMax.to($( this ), 0.5, {scale:2, transformOrigin:("100% 100%"), delay:0});
+  }
+);
+$( ".cornerImg" ).mouseout(
+  // console.log('hello');
+    function() {
+    TweenMax.to($( this ), 0.3, {scale:0.5, transformOrigin:("100% 100%")});
+    TweenMax.to($( this ), 0.1, {alpha:0, delay:0.2});
+  }
+);
+
+$( ".dayImg" ).click(
+  // console.log('hello');
+    function() {
+    TweenMax.to($( this ), 0.3, {alpha:0, delay:0});
   }
 );
